b914b39d9a8f7d164e4ae5b98e474e052751f52b
Rafael <rafaelmoura135790@gmail.com>
2024-09-22 13:49:25 -0300

    add custom splash screen for lower android devices compatibility

--- app/src/main/java/com/rafael/inclusimap/MainActivity.kt
+++ app/src/main/java/com/rafael/inclusimap/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationSettingsRequest
 import com.google.android.gms.location.Priority
@@ -17,8 +18,9 @@ import com.rafael.inclusimap.ui.theme.InclusiMapTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         enableLocation()
         val driveService = GoogleDriveService()
         setContent {
