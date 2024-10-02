0a2e298ef13531b39d24694324b0c5eb8ed7d9ca
Rafael <rafaelmoura135790@gmail.com>
2024-10-01 21:54:19 -0300

    fix UI for MapTypeSelection

--- feature/settings/src/main/kotlin/com/rafael/inclusimap/feature/settings/presentation/components/templates/MultiSelectionPreference.kt
+++ feature/settings/src/main/kotlin/com/rafael/inclusimap/feature/settings/presentation/components/templates/MultiSelectionPreference.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -45,21 +46,23 @@ internal fun MultiSelectionPreference(
             .padding(horizontal = 10.dp),
     ) {
         LazyHorizontalStaggeredGrid(
-            rows = StaggeredGridCells.Adaptive(108.dp),
+            rows = StaggeredGridCells.Adaptive(100.dp),
             modifier = Modifier
                 .fillMaxWidth(),
+            contentPadding = PaddingValues(horizontal = 2.dp, vertical = 6.dp),
         ) {
             selections.forEach {
                 item {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(6.dp),
                         modifier = Modifier
-                            .padding(horizontal = 8.dp, vertical = 8.dp)
                             .animateContentSize()
                             .fillMaxWidth()
                             .height(30.dp)
-                            .clip(RoundedCornerShape(12.dp))
-                            .border(width = 1.dp, color = MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(12.dp))
+                            .padding(horizontal = 4.dp)
+                            .clip(RoundedCornerShape(14.dp))
+                            .border(width = 1.dp, color = MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(14.dp))
                             .then(
                                 if (it.type == selected) {
                                     Modifier.background(MaterialTheme.colorScheme.surfaceColorAtElevation(100.dp))
@@ -74,17 +77,18 @@ internal fun MultiSelectionPreference(
                                 imageVector = Icons.TwoTone.Check,
                                 contentDescription = "Icon",
                                 tint = MaterialTheme.colorScheme.onSurface,
+                                modifier = Modifier.padding(start = 4.dp),
                             )
                         }
                         Text(
                             text = it.name,
-                            modifier = Modifier.padding(start = 8.dp),
+                            modifier = Modifier.padding(start = if (it.type != selected) 4.dp else 0.dp),
                         )
                         Icon(
                             imageVector = it.icon,
                             contentDescription = "Icon",
                             tint = MaterialTheme.colorScheme.onSurface,
-                            modifier = Modifier.padding(horizontal = 8.dp),
+                            modifier = Modifier.padding(end = 4.dp),
                         )
                     }
                 }
