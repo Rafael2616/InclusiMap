f97fba77ac5414ffbb9da599a69cf0b1d8d8b0c7
Rafael <rafaelmoura135790@gmail.com>
2024-10-04 21:18:35 -0300

    use the versions defined in version catalog in app build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,10 +17,10 @@ android {
 
     defaultConfig {
         applicationId = "com.rafael.inclusimap"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
