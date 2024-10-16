8468a437ae1b23ffe480ab68b0144e977cfd3596
Rafael <rafaelmoura135790@gmail.com>
2024-10-15 21:09:50 -0300

    reduce the height for NavBar

--- feature/map/src/main/kotlin/com/rafael/inclusimap/feature/map/presentation/InclusimapScaffold.kt
+++ feature/map/src/main/kotlin/com/rafael/inclusimap/feature/map/presentation/InclusimapScaffold.kt
@@ -2,6 +2,7 @@ package com.rafael.inclusimap.feature.map.presentation
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Explore
 import androidx.compose.material.icons.filled.Search
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.MapType
 import com.rafael.inclusimap.core.settings.domain.model.SettingsState
@@ -80,7 +82,10 @@ fun InclusiMapScaffold(
         },
         bottomBar =  {
             if (!isFullScreenMode) {
-                NavigationBar {
+                NavigationBar(
+                    modifier = Modifier
+                        .height(90.dp)
+                ) {
                     items.forEach { item ->
                         NavigationBarItem(
                             selected = item.selected,
