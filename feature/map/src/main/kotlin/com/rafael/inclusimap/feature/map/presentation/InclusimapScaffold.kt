28d0bf8bb2c1ddb8ccb645016450e55e36753b7e
Rafael <rafaelmoura135790@gmail.com>
2024-10-15 21:26:58 -0300

    revert last commit

--- feature/map/src/main/kotlin/com/rafael/inclusimap/feature/map/presentation/InclusimapScaffold.kt
+++ feature/map/src/main/kotlin/com/rafael/inclusimap/feature/map/presentation/InclusimapScaffold.kt
@@ -2,7 +2,6 @@ package com.rafael.inclusimap.feature.map.presentation
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Explore
 import androidx.compose.material.icons.filled.Search
@@ -14,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.MapType
 import com.rafael.inclusimap.core.settings.domain.model.SettingsState
@@ -82,10 +80,7 @@ fun InclusiMapScaffold(
         },
         bottomBar =  {
             if (!isFullScreenMode) {
-                NavigationBar(
-                    modifier = Modifier
-                        .height(90.dp)
-                ) {
+                NavigationBar {
                     items.forEach { item ->
                         NavigationBarItem(
                             selected = item.selected,
