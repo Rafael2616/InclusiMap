69d24903edca8638facf7e5d9ca51fa142493475
Rafael <rafaelmoura135790@gmail.com>
2024-10-23 12:37:10 -0300

    fix build

--- feature/map/src/main/kotlin/com/rafael/inclusimap/feature/map/map/presentation/dialog/DeletePlaceConfirmationDialog.kt
+++ feature/map/src/main/kotlin/com/rafael/inclusimap/feature/map/map/presentation/dialog/DeletePlaceConfirmationDialog.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -17,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -77,7 +81,7 @@ fun DeletePlaceConfirmationDialog(
                             Text(text = "Manter")
                         }
                         Button(
-                            onClick = onConfirm,
+                            onClick = onDelete,
                         ) {
                             Text(text = "Remover")
                         }
