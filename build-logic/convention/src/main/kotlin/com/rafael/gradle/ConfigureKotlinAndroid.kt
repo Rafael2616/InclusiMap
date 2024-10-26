3feeb36d04cec1ab45d25cf5cd0eaa6d687ec194
Rafael <rafaelmoura135790@gmail.com>
2024-10-25 22:46:01 -0300

    build: use consistent jdk version in project and android studio

--- build-logic/convention/src/main/kotlin/com/rafael/gradle/ConfigureKotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/rafael/gradle/ConfigureKotlinAndroid.kt
@@ -1,10 +1,8 @@
 package com.rafael.gradle
 
 import com.android.build.api.dsl.CommonExtension
-import com.diffplug.gradle.spotless.KotlinExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
@@ -42,10 +40,6 @@ internal fun Project.configureKotlinAndroid(
             jvmTarget.set(JvmTarget.JVM_21)
         }
     }
-}
-
-fun Project.kotlinExtension(block: KotlinExtension.() -> Unit) {
-    extensions.configure<KotlinExtension>(block)
 }
 
 val Project.javaVersion: JavaVersion
